3772c2cdb239f3f148efc679e05b533341e1ad8a
Gael PORT <gael.port@rbs.fr>
2010-07-09 16:15:07 +0200

    [NEW] Dynamic list of shops. [NEW] Replace the shop picker in prices by a drop down list.

---
+++ lib/services/ListShopsService.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package module.catalog
+ */
+class catalog_ListShopsService extends BaseService
+{
+	/**
+	 * @var catalog_ListShopsService
+	 */
+	private static $instance;
+
+	/**
+	 * @return catalog_ListShopsService
+	 */
+	public static function getInstance()
+	{
+		if (is_null(self::$instance))
+		{
+			self::$instance = self::getServiceClassInstance(get_class());
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * @return array<list_Item>
+	 */
+	public final function getItems()
+	{
+		$items = array();
+		foreach (catalog_ShopService::getInstance()->createQuery()->find() as $shop)
+		{
+			$items[] = new list_Item(
+				$shop->getLabel(),
+				$shop->getId()
+			);
+		}
+		return $items;
+	}
+}
