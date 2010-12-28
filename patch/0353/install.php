4ccd19420255c77c0036e7339335161700a8d71d
Gaël PORT <gael.port@rbs.fr>
2010-12-28 16:50:01 +0100

    [FIX] Fix patch 0353.

--- patch/0353/install.php
+++ patch/0353/install.php
@@ -5,28 +5,16 @@
  */
 class catalog_patch_0353 extends patch_BasePatch
 {
-//  by default, isCodePatch() returns false.
-//  decomment the following if your patch modify code instead of the database structure or content.
-    /**
-     * Returns true if the patch modify code that is versionned.
-     * If your patch modify code that is versionned AND database structure or content,
-     * you must split it into two different patches.
-     * @return Boolean true if the patch modify code that is versionned.
-     */
-//	public function isCodePatch()
-//	{
-//		return true;
-//	}
- 
 	/**
 	 * Entry point of the patch execution.
 	 */
 	public function execute()
 	{
 		// Implement your patch here.
-		$this->executeLocalXmlScript('taxes.xml');
-		$this->executeLocalXmlScript('currencies.xml');
-		// Rajouter le currencyId
+		$this->executeModuleScript('taxes.xml', 'catalog');
+		$this->executeModuleScript('currencies.xml', 'catalog');
+		
+		// Rajouter le currencyId.
 		$newPath = f_util_FileUtils::buildWebeditPath('modules/catalog/persistentdocument/price.xml');
 		$newModel = generator_PersistentModel::loadModelFromString(f_util_FileUtils::read($newPath), 'catalog', 'price');
 		$newProp = $newModel->getPropertyByName('currencyId');
