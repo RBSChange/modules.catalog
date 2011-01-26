aee21009fc98a249ad242c38ca7af9e8d704b2f9
Alexandre SIMON <alexandre.simon@rbs.fr>
2011-01-26 17:25:09 +0100

    [FIX #26147] addStockQuantity

---
+++ patch/0312/install.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * catalog_patch_0312
+ * @package modules.catalog
+ */
+class catalog_patch_0312 extends patch_BasePatch
+{
+//  by default, isCodePatch() returns false.
+//  decomment the following if your patch modify code instead of the database structure or content.
+    /**
+     * Returns true if the patch modify code that is versionned.
+     * If your patch modify code that is versionned AND database structure or content,
+     * you must split it into two different patches.
+     * @return Boolean true if the patch modify code that is versionned.
+     */
+//	public function isCodePatch()
+//	{
+//		return true;
+//	}
+ 
+	/**
+	 * Entry point of the patch execution.
+	 */
+	public function execute()
+	{
+		$this->execChangeCommand('compile-documents', array());
+	}
+
+	/**
+	 * @return String
+	 */
+	protected final function getModuleName()
+	{
+		return 'catalog';
+	}
+
+	/**
+	 * @return String
+	 */
+	protected final function getNumber()
+	{
+		return '0312';
+	}
+}
