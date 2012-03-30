ea7e097f43489320ef274eb13b21db61c9f8f90e
Gaël PORT <gael.port@rbs.fr>
2012-03-30 17:57:04 +0200

    [REFACTOR] getTreeNodeLabel

--- persistentdocument/billingareafolder.class.php
+++ persistentdocument/billingareafolder.class.php
@@ -5,16 +5,4 @@
  */
 class catalog_persistentdocument_billingareafolder extends catalog_persistentdocument_billingareafolderbase 
 {	
-	/**
-	 * @return string
-	 */
-	public function getTreeNodeLabel()
-	{
-		$label = parent::getTreeNodeLabel();
-		if ($label === 'billingareafolder')
-		{
-			return LocaleService::getInstance()->transBO($this->getPersistentModel()->getLabelKey(), array('ucf'));
-		}
-		return $label;
-	}
 }
