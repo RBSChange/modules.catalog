09e68b869a88dcc875f6e6150a38988066372d14
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-08-02 12:30:21 +0200

    [FIX #62506] Import XML des prix et stockage des prix HT/TTC

--- persistentdocument/import/LockedpriceScriptDocumentElement.class.php
+++ persistentdocument/import/LockedpriceScriptDocumentElement.class.php
@@ -3,8 +3,17 @@
  * catalog_LockedpriceScriptDocumentElement
  * @package modules.catalog.persistentdocument.import
  */
-class catalog_LockedpriceScriptDocumentElement extends import_ScriptDocumentElement
+class catalog_LockedpriceScriptDocumentElement extends catalog_PriceScriptDocumentElement
 {
+	
+	/**
+	 * @return catalog_LockedpriceService
+	 */
+	protected function getDocumentService()
+	{
+		return catalog_LockedpriceService::getInstance();
+	}
+	
     /**
      * @return catalog_persistentdocument_lockedprice
      */
